595b51603a8626fb489edc9be37a7ccab42ace82
Venkat Jammalamudi <venkat.jammalamudi@vorboss.com>
2022-09-27 23:03:51 +0100

    Added github actions"

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v(36xb%zb3_l_*)@iqnf(19#rq=h#nyl9lj7je2erbiuz%c8_*'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
